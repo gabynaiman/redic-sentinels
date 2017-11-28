c1954672c1b08658d552ccae61a5d449181587b5
Gabriel Naiman <gnaiman@keepcon.com>
2017-11-28 15:47:09 -0300

    Refactor tests

--- spec/sentinels_spec.rb
+++ spec/sentinels_spec.rb
@@ -23,98 +23,96 @@ describe Redic::Sentinels do
 
   describe 'Connection success' do
 
-    before do
-      @redis = Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME
-    end
+    let(:redis) { Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME }
 
+    let(:sentinel) { Redic.new "redis://#{VALID_HOSTS.first}" }
+    
     after do
-      ip, port = sentinel.call 'SENTINEL', 'get-master-addr-by-name', MASTER_NAME
+      _, port = sentinel.call 'SENTINEL', 'get-master-addr-by-name', MASTER_NAME
       master = Redic.new "redis://localhost:#{port}"
       master.call! 'FLUSHDB'
     end
 
-    let(:sentinel) { sentinel = Redic.new "redis://#{VALID_HOSTS.first}" }
-
     it 'call' do
-      @redis.call('PING').must_equal 'PONG'
-      @redis.call('INVALID_COMMAND').must_be_instance_of RuntimeError
+      redis.call('PING').must_equal 'PONG'
+      redis.call('INVALID_COMMAND').must_be_instance_of RuntimeError
     end
 
     it 'call!' do
-      @redis.call!('PING').must_equal 'PONG'
-      proc { @redis.call! 'INVALID_COMMAND' }.must_raise RuntimeError
+      redis.call!('PING').must_equal 'PONG'
+      proc { redis.call! 'INVALID_COMMAND' }.must_raise RuntimeError
     end
 
     it 'queue/commit' do
-      @redis.queue('PING').must_equal [['PING']]
-      @redis.queue('PING').must_equal [['PING'], ['PING']]
-      @redis.commit.must_equal ['PONG', 'PONG']
+      redis.queue('PING').must_equal [['PING']]
+      redis.queue('PING').must_equal [['PING'], ['PING']]
+      redis.commit.must_equal ['PONG', 'PONG']
     end
 
     it 'clear/reset' do
-      @redis.queue 'PING'
-      @redis.buffer.must_equal [['PING']]
+      redis.queue 'PING'
+      redis.buffer.must_equal [['PING']]
 
-      @redis.clear
-      @redis.buffer.must_be_empty
+      redis.clear
+      redis.buffer.must_be_empty
 
-      @redis.queue 'PING'
-      @redis.buffer.must_equal [['PING']]
+      redis.queue 'PING'
+      redis.buffer.must_equal [['PING']]
 
-      @redis.reset
-      @redis.buffer.must_be_empty
+      redis.reset
+      redis.buffer.must_be_empty
     end
 
     it 'get/set' do
-      @redis.call!('GET', 'key1').must_be_nil
-      @redis.call! 'SET', 'key1', 'value1'
-      @redis.call!('GET', 'key1').must_equal 'value1'
+      redis.call!('GET', 'key1').must_be_nil
+      redis.call! 'SET', 'key1', 'value1'
+      redis.call!('GET', 'key1').must_equal 'value1'
     end
 
     it 'Retry on connection failures' do
-      @redis.call('PING').must_equal 'PONG'
+      redis.call('PING').must_equal 'PONG'
 
-      disconnect @redis
+      disconnect redis
 
-      @redis.call('PING').must_equal 'PONG'
-      @redis.queue('PING').must_equal [['PING']]
+      redis.call('PING').must_equal 'PONG'
+      redis.queue('PING').must_equal [['PING']]
 
-      disconnect @redis
+      disconnect redis
 
-      @redis.queue('PING').must_equal [['PING'], ['PING']]
-      @redis.commit.must_equal ['PONG', 'PONG']
+      redis.queue('PING').must_equal [['PING'], ['PING']]
+      redis.commit.must_equal ['PONG', 'PONG']
     end
 
     it 'Retry on Master change' do
-      @redis.call('PING').must_equal 'PONG'
+      redis.call('PING').must_equal 'PONG'
 
-      @redis.call('PING').must_equal 'PONG'
-      @redis.queue('PING').must_equal [['PING']]
+      redis.call('PING').must_equal 'PONG'
+      redis.queue('PING').must_equal [['PING']]
 
       sentinel.call 'SENTINEL', 'failover', 'mymaster'
 
-      @redis.call('PING').must_equal 'PONG'
-      @redis.queue('PING').must_equal [['PING'], ['PING']]
+      redis.call('PING').must_equal 'PONG'
+      redis.queue('PING').must_equal [['PING'], ['PING']]
 
-      @redis.queue('PING').must_equal [['PING'], ['PING'], ['PING']]
-      @redis.commit.must_equal ['PONG', 'PONG', 'PONG']
+      redis.queue('PING').must_equal [['PING'], ['PING'], ['PING']]
+      redis.commit.must_equal ['PONG', 'PONG', 'PONG']
     end
 
     it 'Default DB' do
       ip, port = sentinel.call 'SENTINEL', 'get-master-addr-by-name', MASTER_NAME
-      @redis.url.must_equal "redis://#{ip}:#{port}/0"
+      redis.url.must_equal "redis://#{ip}:#{port}/0"
     end
 
     it 'Custom DB' do
       ip, port = sentinel.call 'SENTINEL', 'get-master-addr-by-name', MASTER_NAME
-      @redis = Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME, db: 7
-      @redis.url.must_equal "redis://#{ip}:#{port}/7"
+      redis = Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME, db: 7
+      redis.url.must_equal "redis://#{ip}:#{port}/7"
     end
 
     it 'Auth' do
       ip, port = sentinel.call 'SENTINEL', 'get-master-addr-by-name', MASTER_NAME
-      @redis = Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME, password: 'pass'
-      @redis.url.must_equal "redis://:pass@#{ip}:#{port}/0"
+      redis = Redic::Sentinels.new hosts: HOSTS, master_name: MASTER_NAME, password: 'pass'
+      redis.url.must_equal "redis://:pass@#{ip}:#{port}/0"
     end
 
   end
